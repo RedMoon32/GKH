7cef23fa9f46df6c0d96cf10047837426cfeea3d
RedMoon32 <rinat.babichev@gmail.com>
2019-06-15 20:59:03 +0300

    upload from organisation

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -1,17 +1,65 @@
 # Todo вынести переменную сессию бота в отдельный файл
 from vk_api.utils import get_random_id
 
-from core.models import UserStatuses
+from core.models import UserStatuses, Organisation, CSV_File
 from src.bot.messages import *
+import urllib.request
+import os
+from GKH.settings import *
 
 
 def start(vk, event, user, session):
-    for mess in [WELCOME, ENTER_NAME]:
+    for mess in [WELCOME, IS_ORGANISATION]:
         vk.messages.send(
             user_id=event.obj.from_id,
             random_id=get_random_id(),
             message=mess)
-    session.status = UserStatuses.enter_name
+    session.status = UserStatuses.enter_role
+
+
+def enter_role(vk, event, user, session):
+    if event.obj.text.lower() == "да":
+        org = Organisation.objects.get_or_create(vk_id=event.obj.from_id)[0]
+        org.save()
+        session.status = UserStatuses.enter_org_name
+        vk.messages.send(
+            user_id=event.obj.from_id,
+            random_id=get_random_id(),
+            message=ORGANISATION_NAME)
+    else:
+        session.status = UserStatuses.enter_name
+
+
+def enter_org_name(vk, event, user, session):
+    org = Organisation.objects.get_or_create(vk_id=event.obj.from_id)[0]
+    org.name = event.obj.text
+    org.save()
+    session.status = UserStatuses.allowed_group
+    vk.messages.send(
+        user_id=event.obj.from_id,
+        random_id=get_random_id(),
+        message=FILES)
+
+
+def receive_file(vk, event, user, session):
+    if len(event.obj.attachments) == 0:
+        vk.messages.send(
+            user_id=event.obj.from_id,
+            random_id=get_random_id(),
+            message='Пожалуйста приложите файл к сообщению')
+    else:
+        new = CSV_File.objects.create()
+        org = Organisation.objects.get(vk_id=event.obj.from_id)
+        new.file_path = os.path.join(BASE_DIR, "files") + f"/{new.id}.csv"
+        open(new.file_path, "w")
+        urllib.request.urlretrieve(event.obj.attachments[0]["doc"]["url"], new.file_path)
+        org.files.add(new)
+        org.save()
+        new.save()
+        vk.messages.send(
+            user_id=event.obj.from_id,
+            random_id=get_random_id(),
+            message='Показания счетчиков загружены для просмотра пользователей')
 
 
 def enter_address(vk, event, user, session):
@@ -32,7 +80,10 @@ def get_help(vk, event, user, session):
 
 def downgrade(vk, event, user, session):
     session.status = UserStatuses.start
-    start(vk, event, user, session)
+    vk.messages.send(
+        user_id=event.obj.from_id,
+        random_id=get_random_id(),
+        message=ENTER_NAME)
 
 
 def get_data(vk, event, user, session):
@@ -49,7 +100,3 @@ def enter_name(vk, event, user, session):
         random_id=get_random_id(),
         message=ENTER_ADDRESS)
     session.status = UserStatuses.enter_address
-
-
-def send__message():
-    pass
--- src/core/models.py
+++ src/core/models.py
@@ -7,8 +7,20 @@ class UserStatuses(DjangoChoices):
     enter_name = ChoiceItem()
     start = ChoiceItem()
     allowed = ChoiceItem()
-
+    enter_org_name = ChoiceItem()
+    enter_role = ChoiceItem()
+    allowed_group = ChoiceItem()
     # enter_family = ChoiceItem()
+
+
+class CSV_File(models.Model):
+    file_path = models.CharField(null=True, max_length=100)
+
+
+class Organisation(models.Model):
+    vk_id = models.IntegerField()
+    name = models.CharField(max_length=100, null=True, blank=True)
+    files = models.ManyToManyField(CSV_File)
 
 
 class UserData(models.Model):
@@ -19,4 +31,4 @@ class UserData(models.Model):
 
 class VkSession(models.Model):
     user = models.ForeignKey(UserData, null=True, blank=True, on_delete=models.SET_NULL)
-    status = models.CharField(max_length=100, choices=UserStatuses.choices, default=UserStatuses.start)
+    status = models.CharField(max_length=100, choices=UserStatuses.choices, default=UserStatuses.enter_role)
